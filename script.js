88459b68d29a06431f17fb4bf85d0c7303164c1f
Manish <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-10 16:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
-//complete this code
+// //complete this code
+// class Animal {
+// 	constructor(species){
+// 		this._species = species;
+// 	}
+// 	get species_get(){
+// 		return this._species;
+// 	}
+// 	makeSound(){
+// 		console.log(`The ${this._species} makes a sound`);
+// 	}
+// }
+
+// class Dog extends Animal {
+// 	purr(){
+// 		console.log('purr');
+// 	}
+// }
+
+// class Cat extends Animal {
+// 	bark(){
+// 		console.log('woof');
+// 	}
+// }
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+
+
 class Animal {
-	constructor(species){
+	constructor(species) {
 		this._species = species;
 	}
-	get species_get(){
+
+	get species() {
 		return this._species;
 	}
-	makeSound(){
+
+	makeSound() {
 		console.log(`The ${this._species} makes a sound`);
 	}
 }
 
-class Dog extends Animal {
-	purr(){
-		console.log('purr');
+class Cat extends Animal {
+	purr() {
+		console.log("purr");
 	}
 }
 
-class Cat extends Animal {
-	bark(){
-		console.log('woof');
+class Dog extends Animal {
+	bark() {
+		console.log("woof");
 	}
 }
 
